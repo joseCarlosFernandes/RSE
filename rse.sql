c5c70d380ee2d234cb57e3642cf6ca51a8e54f90
José Carlos Fernandes Filho <149443806+joseCarlosFernandes@users.noreply.github.com>
2023-10-30 22:34:43 -0300

    Estrutura do banco de dados do site
    Sem nenhum registro e somente com uma tabela

---
+++ rse.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 31-Out-2023 às 02:34
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `rse`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios_rse`
+--
+
+CREATE TABLE `usuarios_rse` (
+  `email` char(50) NOT NULL,
+  `nome` char(50) NOT NULL,
+  `senha` varchar(128) NOT NULL,
+  `img` longblob DEFAULT NULL,
+  `tipoImg` varchar(10) DEFAULT NULL,
+  `descricao` varchar(150) DEFAULT 'Olá sou novo aqui!',
+  `data_criacao` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuarios_rse`
+--
+ALTER TABLE `usuarios_rse`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
